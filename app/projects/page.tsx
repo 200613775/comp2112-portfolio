b47e6b70bca7ce01c6c9a5235af2e4d92c8eb575
200613775 <patelnishi369@gmail.com>
2025-10-05 14:45:36 -0400

    Save changes

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image'
 
 const projects = [
-    { title: 'Logo Design', desc: 'In this one, I have created a logo for the website using illustrator in my first semister.', img: '/Project-1.png' },
-    { title: 'Song Lyrics', desc: 'In this project, I have created a webpage where I added a song lyrics in HTMl and through CSS I have done styling for the words, and added background image as well. In footer I have added a link for the actual lyrics website.', img: '/project-2.png' },
-    { title: 'Book Search', desc: 'I have build a website for book search by writer using API in JavaScript.', img: '/project1.png' } ]
+    { title: 'Logo Design', desc: 'In this one, I have created a logo for the website using illustrator in my first semister.', img: '/projectimg/Project-1.png'},
+    { title: 'Song Lyrics', desc: 'In this project, I have created a webpage where I added a song lyrics in HTMl and through CSS I have done styling for the words, and added background image as well. In footer I have added a link for the actual lyrics website.', img: '/projectimg/Project-2.png' },
+    { title: 'Book Search', desc: 'I have build a website for book search by writer using API in JavaScript.', img: '/projectimg/Project-3.png' } ]
 
 export default function Projects() {
   return (
